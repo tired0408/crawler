1f3063bf85d5510f0f943044fbf10c925601f135
hujt <89085657@qq.com>
2025-09-10 15:09:29 +0800

    fix bug

--- crawler/zherun/auto.bat
+++ crawler/zherun/auto.bat
@@ -1,3 +1,8 @@
+@echo off
+REM =======================================
+REM 程序功能：用于window启动python程序的脚本
+REM 定时开关：Win + R键，输入taskschd.msc，小红书抓取名称
+REM =======================================
 cd E:\py-workspace\crawler\crawler\zherun
 C:\Users\Administrator\miniforge3\envs\py310\python.exe xiaohongshu.py
 pause
